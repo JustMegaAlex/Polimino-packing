3e0970c7421059f53f937a9133dafd08f176156f
JustMeagaAlex <alex_govr@mail.ru>
2019-10-22 09:50:05 +0300

    рабочий вариант, алгоритм без откатов

---
+++ polimino_packing.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3.6
+# -*- coding: UTF-8 -*-
+
+import copy
+
+try:
+    from local_config import DEBUG
+except:
+    DEBUG = False
+
+class Error(BaseException):
+    def __init__(self, mess, signal = False):
+        self.message = 'Error: ' + mess
+        self.signal = signal
+        
+class Polimino:
+    var = 2
+    def __init__(self, width, height, num, kind):
+        self.width = width
+        self.height = height          
+        self.kind = kind    # 'R' - rectangular, 'L' - L-kind, 'S' - square
+        self.num = num      # polimino power (number of them on the table)
+
+        self.image = {}     # образ полимино, для добавления в table
+        k = 0
+        if(kind == 'R'):                     # прямоугольный
+            if(height==width):kind = 'S'
+            if self.height > self.width:
+                self.width, self.height = self.height, self.width
+            for i in range(self.height):
+                for j in range(self.width):
+                    self.image[k] = [i, j]
+                    k += 1
+        else:                               # L-образный
+            self.image[k] = [0, 0]
+            k=1
+            for j in range(1, width):
+                self.image[k] = [0, j]
+                k += 1
+            for i in range(1, height):
+                self.image[k] = [i, 0]
+                k += 1
+        
+    def printInfo(self):
+        print('w='+str(self.width)+' h='+str(self.height)+' num='+str(self.num)+' kind='+self.kind)
+    
+    def print_image(self):
+        if self.kind == 'R':
+            s = ('# '*self.width + '\n')*self.height
+        else:
+            s = '# '*self.width + '\n# '*(self.height-1) + '\n'
+        print(s)
+        
+    def rotated(self, rotation):
+        if not rotation:
+            return self.image
+        rot_image = copy.deepcopy(self.image)
+        if rotation==1 :
+            for k in range(len(self.image)):
+                # i_rot = -j + w - 1
+                rot_image[k][0] = -self.image[k][1] + self.width - 1
+                # j_rot = i
+                rot_image[k][1] = self.image[k][0]
+        elif rotation==2 :
+            for k in range(len(self.image)):
+                # i_rot = -i + h - 1
+                rot_image[k][0] = -self.image[k][0] + self.height - 1
+                # j_rot = -j + w - 1
+                rot_image[k][1] = -self.image[k][1] + self.width - 1
+        elif rotation==3 :
+            for k in range(len(self.image)):
+                # i_rot = j
+                rot_image[k][0] = self.image[k][1]
+                # j_rot = -i + h - 1
+                rot_image[k][1] = -self.image[k][0] + self.height - 1
+        else: 
+            raise Error('Polimino.rotated: wrong rotation argument')
+        return rot_image
+    
+class Table:
+    def __init__(self, width=1, height=1, rows_involved=1, rows_packed=0):
+        self.width = width                          # ширина стола
+        self.height = height                        # высота стола   
+        self.rows_involved = rows_involved    # сколько рядов затронуто при размещении
+        self.rows_packed = rows_packed        # сколько рядов заполнено
+        self.table = []                     # сетка стола
+        self.ini_table(width, height)
+        self.area = width*height
+        self.track_list = []
+
+    def ini_table(self, width, height):
+        if width<0 or height<0:
+             raise Error('Отрицательные размеры стола')
+        self.table = [0]*height
+        for i in range(height): 
+            self.table[i] = [0]*width
+
+    def quality_factor(self):
+        factor = 0
+        for i in range(self.rows_involved):
+            row_factor = self.table[i][0]==0
+            for j in range(1, self.width):
+                row_factor += (self.table[i][j]==0)+((self.table[i][j]==1) & (self.table[i][j-1]==0))*0.5
+            factor += row_factor * (1 + 0.25*(self.rows_involved - i - 1))
+        return factor/self.rows_involved
+    
+    def print_table(self):
+        s = ''
+        for i in range(self.rows_involved):
+            for j in range(self.width): s += str(self.table[i][j])
+            s += '\n'
+        s += ('0'*self.width + '\n')*(self.height - self.rows_involved)
+        print(s)
+
+    def place_polimino(self, i, j, polim, rotation = 0):
+        # в пределах стола?
+        if rotation in [0, 2]:
+            if((i+polim.height)>self.height or (j+polim.width)>self.width):
+                # print('outside the table')
+                return  False
+        else:
+            if((i+polim.width)>self.height or (j+polim.height)>self.width):
+                # print('outside the table')
+                return  False
+
+        # вставка образа в table
+        image = polim.rotated(rotation)
+        for k in range(len(image)): # image - это словарь
+            # координаты клеток,  размещаемых на столе
+            ii = i + image[k][0]   
+            jj = j + image[k][1]
+            if self.table[ii][jj]:
+                self.undo(i, j, image, k)
+                # print(str(ii)+' '+str(jj))
+                return False
+            self.table[ii][jj] = 1
+            self.rows_involved = max(ii+1, self.rows_involved)
+        polim.num -= 1
+        # print('polimono placed num='+str(polim.num))
+        self.track_list.append([polim, i, j, rotation, self.rows_involved])
+        #print()
+        debug_message('rows '+str(self.rows_involved),False)
+        # self.print()
+        return True
+    
+    def undo(self, i, j, image, k):
+        for kk in range(k):
+            ii = i + image[kk][0]
+            jj = j + image[kk][1]
+            self.table[ii][jj] = 0
+    
+    def undo_placement(self):
+        [polim, i, j, rotation] = self.track_list.pop()[:4]
+        self.rows_involved = self.track_list[-1][4]
+        debug_message('undo:rows '+str(self.rows_involved),False)
+        polim.num += 1
+        self.undo(i, j, polim.rotated(rotation), len(polim.image))
+        return i, j
+
+def debug_message(s,wait):
+    if(DEBUG):
+        print(s)
+        if(wait):
+            input()
--- task.py
+++ task.py
@@ -1,105 +1,195 @@
-class Error(BaseException):
-    def __init__(self,mess):
-        self.message = 'Error: ' + mess
-        pass
+#!/usr/bin/env python3.6
+# -*- coding: UTF-8 -*-
 
-class Polimino:
-    var = 2
-    def __init__(self,w,h,num,type):
-        self.w = w
-        self.h = h          
-        self.type = type    #'R' - rectangular, 'L' - L-type, 'S' - square
-        self.num = num      #polimino power (number of them on the table)
+from polimino_packing import Error, Polimino, Table
 
-        self.image = {}     #образ полимино
-        k = 0
-        if(type == 'R'):
-            if(h==w):type = 'S'
-            if(self.h > self.w):
-                buf = self.h
-                self.h = self.w
-                self.w = buf
-            for i in range(w):
-                for j in range(h):
-                    self.image[k] = [i,j]
-                    k += 1
-        else:
-            self.image[k] = [0,0]
-            k=1
-            for i in range(1,w):
-                self.image[k] = [i,0]
-                k += 1
-            for i in range(1,h):
-                self.image[k] = [0,i]
-                k += 1
-        pass
-
-    def printInfo(self):
-        print('w='+str(self.w)+' h='+str(self.h)+' num='+str(self.num)+' type='+self.type)
-        pass
-    def printImage(self):
-        listTemp = []
-        for i in range(len(self.image)):
-            listTemp[self.image[i][0]][self.image[i][1]] = '#'
-        s = ''
-        for i in range(len(self.image)):
-            s += '\n'*self.image[i][1]+' '*self.image[i][0]+'#'
-        print(s)
-        pass
-    
-class Table:
-    def __init__(self,w=1,h=1,whiteCells=0,rowsInvolved=0,rowsPacked=0):
-        self.w = w                          #ширина стола
-        self.h = h                          #высота стола
-        self.whiteCells = whiteCells                  #пропуски клеток в рядах при размещении полимино
-        self.rowsInvolved = rowsInvolved    #сколько рядов затронуто при размещении
-        self.rowsPacked = rowsPacked        #сколько рядов заполнено
-        self.table = []                     #сетка стола
-        self.iniTable(w,h)
-        pass
-
-    def iniTable(self,w,h):
-        if(w<0 or h<0): raise Error('Отрицательные размеры стола')
-        row = []
-        [row.append(0) for i in range(w)]
-        [self.table.append(row) for j in range(h)]
-        pass
-
-    def qualityFactor(self):
-        factor = 0
-        for i in range(self.rowsInvolved):
-            factor += self.table[i][0]==0
-            for j in range(1,self.w):
-                factor += (self.table[i][j]==0)+((self.table[i][j]==1) & (self.table[i][j-1]==0))*0.5
-        return factor
-
-    # a comment
 try:
-    # initialize task
-    iniTaskList = [
-        [4,4],                  #table size [w,h]
-        [((1,1),2),((2,2),2)],  #square poliminos [((w,h),N)...]
-        [((5,4),2)]                      #L-poliminos [((w,h),N)...]
+    from local_config import ini_task_list, DEBUG
+except:
+    DEBUG = False
+    ini_task_list = [
+        [4, 5],             # размеры стола[w, h]    
+        
+        [                   # прямоугольные полимино[((w, h), N)...]
+            ((2, 2), 1)
+        ], 
+                      
+        [                   # L-полимино [((w, h), N)...]
+            ((2, 3), 1), 
+            ((2, 2), 1), 
+            ((3, 2), 1), 
+            ((3, 3), 1)
+        ]    
     ]
-    #создаем стол
-    table = Table(w=iniTaskList[0][0],h=iniTaskList[0][1],rowsInvolved=3)
-    '''table.table = [
-        [1,1,1,1,1,0],
-        [0,1,0,1,0,1],
-        [0,0,1,0,0,0],
-    ]
-    table.w = 6
-    table.h = 3
-    print(table.qualityFactor())'''
-    ###create list of polimino instances
-    #square poliminos
-    listPolim = [Polimino(l[0][0],l[0][1],l[1],'R') for l in iniTaskList[1]]
-    #L-poliminos
-    listPolim += [Polimino(l[0][0],l[0][1],l[1],'L') for l in iniTaskList[2]]
-    listPolim[2].printImage()
-    #check poliminos' sizes are valid
-    for poli in listPolim:
-        if(poli.w>table.w or poli.h>table.h):
-            raise Error('one of poliminos is larger than the table!')
+
+def search_free_cell(i_begin=0, j_begin=0):
+    # переход на следующую строку
+    if j_begin >= len(table_instance.table[0]):
+        j_begin = 0
+        i_begin += 1
+    # поиск свободной ячейки
+    for i in range(i_begin, len(table_instance.table)):
+        j_begin = j_begin*(i==i_begin)
+        for j in range(j_begin, len(table_instance.table[0])):
+            # print(str(i)+' '+str(j)+' '+str(table_instance.table[i][j]))
+            if table_instance.table[i][j]==0:
+                i_place = i
+                j_place = j
+                # print('found '+str(i_place)+' '+str(j_place))
+                return i_place, j_place
+    return -1, -1
+
+def undo_placement():
+    global i_place, j_place 
+    i_place, j_place = table_instance.undo_placement()
+
+def find_next_polim(ind = 0):
+    for k in range(ind,len(POLIMINOS)):
+        if POLIMINOS[k].num:
+            return POLIMINOS[k],k
+    return None, 0
+        
+try:
+    # создаем экземпляр стола
+    table_instance = Table(width=ini_task_list[0][0], height=ini_task_list[0][1])
+    table_instance.print_table() 
+
+    # заполняем список POLIMINOS
+    POLIMINOS = [Polimino(l[0][0], l[0][1], l[1], 'R') for l in ini_task_list[1]]
+    # число прямоугольных полимино
+    # rect_polim_number = len(ini_task_list[1])
+    # L-poliminos
+    POLIMINOS += [Polimino(l[0][0], l[0][1], l[1], 'L') for l in ini_task_list[2]]
+    
+    # общее количество полимино, размещаемых на столе
+    unpacked_polims = 0 
+    for poli in POLIMINOS:
+        unpacked_polims += poli.num
+    print('Poliminos total:'+str(unpacked_polims))
+
+    # проверка:
+    area = 0        # площадь полимино
+    for poli in POLIMINOS:
+        # размеры полимино >= размеров стола
+        if(poli.width>table_instance.width or poli.height>table_instance.height):
+            raise Error('One of poliminos is larger than the table!')
+
+        # общая площадь полимино >= площади стола
+        if poli.kind == 'L':
+            area += (poli.width + poli.height - 1)*poli.num
+        else:
+            area += (poli.width*poli.height)*poli.num
+        print(area)
+        if area > table_instance.area :
+            raise Error('Total polimino area is larger than the table area!')
+    print('Poliminos total area: '+str(area))
+
+    # сортируем полимино по наибольшему размеру: по убыванию 
+    # сначала прямоугольные,  затем L
+    # bubble sort
+    swapped = True
+    while(swapped):
+        swapped = False
+        for k in range(len(POLIMINOS)-1):
+            if max(POLIMINOS[k].width, POLIMINOS[k].height)<max(POLIMINOS[k+1].width, POLIMINOS[k+1].height):
+                POLIMINOS[k], POLIMINOS[k+1] = POLIMINOS[k+1], POLIMINOS[k]
+                swapped = True
+    # print sorting results
+    print('Poliminos are sorted:')
+    [print(el.kind + str(max(el.width, el.height))) for el in POLIMINOS]
+    print('Poliminos total:'+str(unpacked_polims))
+
+    # # # # # размещаем полимино
+    ind = 0
+    # первый полимино размещаем сразу
+    table_instance.place_polimino(0, 0, POLIMINOS[ind], 0)
+    # вывод сетки стола
+    table_instance.print_table()
+    input()
+    unpacked_polims -= 1
+    # начальная клетка
+    i_place = 0
+    j_place = POLIMINOS[ind].width
+    # полимино, при котором фактор будет размещения наименьший
+    best_poli = []      # в списке будет сам полимино и параметры размещения 
+    # задаем начальное значение фактора размещения
+    factor_big_value = table_instance.width * table_instance.height
+    factor = factor_big_value           # заведомо большое значение
+                                        # больше, чем Table.quality_factor может вернуть
+                                        # далее при работе алгоритма чем он меньше, тем лучше 
+    polimino,ind = find_next_polim()
+
+    while unpacked_polims:
+        #print('Trying poli№ '+str(ind)+': num ='+str(polimino.num))
+        # запоминаем начальную клетку
+        i_mem = i_place
+        j_mem = j_place
+        
+        # перебираем все повороты одного полимино
+        # в каждой клетке задействованных строк
+        if polimino:
+            for rotation in range(4):
+                while not(i_place < 0):    # см search_free_cell()
+                    # пробуем разместить
+                    if table_instance.place_polimino(i_place, j_place, polimino, rotation):
+                        new_factor = table_instance.quality_factor()    # новое значение фактора
+                        #print('factor '+str(new_factor))
+                        #table_instance.print_table()
+                        #
+                        # input()
+                        if factor > new_factor:                 # стало лучше?
+                            print('new best poli')
+                            factor = new_factor                                 # запоминаем фактор
+                            best_poli = [i_place, j_place, polimino, rotation]    # и полимино с параметрами размещения
+                        # отменяем размещение для последующего перебора
+                        table_instance.undo_placement()
+                    # print('point('+str(i_place)+', '+str(j_place)+') factor '+str(factor))
+                    # input()
+                    # ищем следующую свободную ячейку
+                    i_place, j_place = search_free_cell(i_place, j_place+1)
+                    # else:print('placement faied')
+                # вспоминаем, откуда начинали
+                i_place = i_mem
+                j_place = j_mem
+        #print('factor ' + str(factor))
+        #polimino.print_image()
+        # переходим к следующему полимино
+        polimino, ind = find_next_polim(ind+1)
+        if not polimino:   # если все полимино проверены
+            print('all polims checked')
+            # если удалось разместить хотя бы один
+            if best_poli:     
+                # выводим образ выбранного полимино     
+                print('poli choosed:')
+                print('factor '+str(new_factor))
+                best_poli[2].print_image()
+                print()
+                # размещаем его окончательно
+                table_instance.place_polimino(best_poli[0], best_poli[1], best_poli[2], best_poli[3])
+                unpacked_polims -= 1
+                # выводим сетку стола
+                table_instance.print_table()
+                # ищем следующую свободную клетку
+                i_place, j_place = search_free_cell(i_place, j_place)
+
+                best_poli = []
+                factor = factor_big_value
+                ind = 0
+                polimino,ind = find_next_polim()
+                input()
+
+            # в противном случае ищем следующую свободную клетку
+            else:
+                #print('next point')
+                i_place, j_place = search_free_cell(i_place, j_place+1)
+                if i_place < 0:
+                    raise Error('Table is full',True)
+                best_poli = []
+                polimino,ind = find_next_polim()
+    # конец алгоритма: успех
+    print('True')
 except Error as e:
     print(e.message)
+    if e.signal:
+        # конец алгоритма: не удалось
+        print('False')
